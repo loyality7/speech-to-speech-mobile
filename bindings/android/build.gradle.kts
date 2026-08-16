1e089d541adafb3f4969fa1298c001d84523377e
Sarath Babu <sarathyadav112@gmail.com>
2026-08-16 15:04:28 +0530

    build(maven): configure maven-publish plugin for SDK release publication (#22)

--- bindings/android/build.gradle.kts
+++ bindings/android/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -22,6 +23,24 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.loyality7"
+                artifactId = "speech-to-speech-mobile"
+                version = "1.0.0"
+            }
+        }
     }
 }
 
